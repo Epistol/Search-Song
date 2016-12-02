6a5d62d013a0d0c8af899e2d5997501508b87616
Epistol <mika63270@live.fr>
2016-12-02 14:53:36 +0100

    Bython

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,6 +6,20 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $route_py = base_path('storage/app/python');
+        $command = escapeshellcmd($route_py.'/Music_Search.py');
+        var_dump($command);
+
+
+        $var = "Get Lucky";
+
+        exec("/usr/bin/python $command 'Get Lucky'", $output, $return);
+
+        var_dump($output);
+
+
+
+
         return view('welcome');
     }
 }
